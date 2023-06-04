592859813e3d9d0351edd9c70b5fdea3c0b29d2e
its-kumar-yash <its.yash.kumar.23@gmail.com>
2023-06-04 18:46:36 +0530

    handling loading state

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   //Fetching data from firebase
@@ -28,9 +29,17 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+  if(isLoading){
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
+
 
   const mealsList = meals.map((meal) => (
     <MealItem
